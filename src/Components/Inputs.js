de1fef15b5951c0d0b4ebd86e21b598634fdfc3b
Raphael Borba <raphaelbsx@gmail.com>
2022-11-15 14:57:27 -0300

    feat: form on SingInpage

--- src/Components/Inputs.js
+++ src/Components/Inputs.js
@@ -2,22 +2,32 @@ import styled from "styled-components";
 import { colors } from "../Assets/GlobalStyles/colors";
 
 
-export default function Input({placeH, type}) {
+export default function Input({ placeH, type, handleForm}) {
 
     return (
 
-        <InputDefault placeholder={placeH} type={type} required/>
+        <InputDefault placeholder={placeH} type={type} required id={type} name={type} onChange={handleForm}/>
     );
 }
 
+
+/* <input
+    required
+    id="email"
+    name="email"
+    value={form.email}
+    onChange={handleForm}
+    type='text'
+    placeholder="email"
+/> */
 
 const InputDefault = styled.input`
 
     width: 90%;
     height: 58px;
-    background-color: ${props => props.type==='submit'?colors.button:'#ffffff'};
+    background-color: ${props => props.type === 'submit' ? colors.button : '#ffffff'};
     font-size: 20px;
-    color: ${props => props.type==='submit'?'#ffffff':'#000000'};
+    color: ${props => props.type === 'submit' ? '#ffffff' : '#000000'};
     border: none;
     border-radius: 5px;
     padding-left: 15px;
--- src/Pages/Sing-in/SingInPage.js
+++ src/Pages/Sing-in/SingInPage.js
@@ -1,11 +1,24 @@
+import { useState } from "react";
 import { PagesDefault } from "../../Assets/GlobalStyles/pagesFormat";
 import Input from "../../Components/Inputs";
 
 
 export default function SingInPage() {
 
+    const [form, setForm] = useState({
+        email:'',
+        password:''
+    })
+
+    function handleForm(e) {
+        const { name, value } = e.target
+        setForm({ ...form, [name]: value })
+    }
+
     function handleSubmit(e){
         e.preventDefault()
+
+
     }
 
     return (
@@ -13,9 +26,9 @@ export default function SingInPage() {
         <PagesDefault>
             <p>MyWallet</p>
             <form onSubmit={handleSubmit} >
-                <Input placeH='E-mail' type='text'/>
-                <Input placeH='Senha' type='password'/>
-                <Input placeH='asdasdaqwe' type='submit' />
+                <Input placeH='E-mail' type='email' handleForm={handleForm} />
+                <Input placeH='Senha' type='password' handleForm={handleForm} />
+                <Input type='submit' />
             </form>
         </PagesDefault>
     );
