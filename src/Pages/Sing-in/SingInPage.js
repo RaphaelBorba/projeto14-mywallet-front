87bc4b53e00bc687aa6793f27d6d7b47247295c5
Raphael Borba <raphaelbsx@gmail.com>
2022-11-15 15:15:56 -0300

    feat: SingUpPage implemented

--- src/Pages/Sing-in/SingInPage.js
+++ src/Pages/Sing-in/SingInPage.js
@@ -1,9 +1,12 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { PagesDefault } from "../../Assets/GlobalStyles/pagesFormat";
 import Input from "../../Components/Inputs";
 
 
 export default function SingInPage() {
+
+    const navigate = useNavigate()
 
     const [form, setForm] = useState({
         email:'',
@@ -13,23 +16,27 @@ export default function SingInPage() {
     function handleForm(e) {
         const { name, value } = e.target
         setForm({ ...form, [name]: value })
+        console.log(form)
     }
 
     function handleSubmit(e){
         e.preventDefault()
 
-
+        
     }
+
 
     return (
 
         <PagesDefault>
             <p>MyWallet</p>
             <form onSubmit={handleSubmit} >
-                <Input placeH='E-mail' type='email' handleForm={handleForm} />
-                <Input placeH='Senha' type='password' handleForm={handleForm} />
+                <Input placeH='E-mail' type='email' handleForm={handleForm} id='email' />
+                <Input placeH='Senha' type='password' handleForm={handleForm} id='password' />
                 <Input type='submit' />
             </form>
+
+            <h1 onClick={()=> navigate('/sing_up')} >Primeira vez? Cadastre-se!</h1>
         </PagesDefault>
     );
 }
