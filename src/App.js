6a3103607b8d4c21cb6f8ac4871c14179ab91c8e
Raphael Borba <raphaelbsx@gmail.com>
2022-11-15 12:25:54 -0300

    feat: React router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import { GlobalStyle } from './Assets/GlobalStyles/ResetCss'
-import {colors} from './Assets/GlobalStyles/colors'
+import { colors } from './Assets/GlobalStyles/colors'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <GlobalStyle colors={colors}/>
-      
+      <BrowserRouter>
+        <GlobalStyle colors={colors} />
+
+        <Routes>
+          <Route path='' element={} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/Assets/GlobalStyles/pagesFormat.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+
+export const PagesDefault = styled.div`
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 400px;
+
+    
+    @media (max-width:400px) {
+        &{
+            width: 100%;
+        }
+    }
+`;
